6de27cc97c7b9b000c33f584b1aba0542ccebac5
lukemm23 <lukemm23@gmail.com>
2020-12-10 21:44:44 -0600

    created a reaction timer

---
+++ src/components/results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction Time: {{score}} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return{
+            rank:null,
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Fastest triggers in the wild wild west!'
+        }else if(this.score < 400){
+            this.rank = 'Mediocre results for a mediocre man.'
+        }else{
+            this.rank = 'Your Dead....'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
